4626116fefa60606dbcc1461b80a456b2560538f
sknskin <sknskin@naver.com>
2022-12-13 18:16:42 +0900

    update map

--- material-dashboard-react2/src/layouts/RentMap/index.js
+++ material-dashboard-react2/src/layouts/RentMap/index.js
@@ -6,17 +6,15 @@ import React, { useEffect } from "react";
 const { kakao } = window;
 
 function Kakao() {
-  
   useEffect(() => {
-
     const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
     const options = {
       // 지도 생성 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+      center: new kakao.maps.LatLng(37.496486063, 127.028361548), // 지도 중심 좌표
       level: 3, // 지도 레벨(확대, 축소 정도)
     };
     const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
-    const markerPosition = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
+    const markerPosition = new kakao.maps.LatLng(37.496486063, 127.028361548);
     const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
@@ -29,8 +27,8 @@ function Kakao() {
       <div
         id="map"
         style={{
-          width: "500px",
-          height: "500px",
+          width: "1600px",
+          height: "800px",
         }}
       />
     </DashboardLayout>
