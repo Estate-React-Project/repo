ab25688e8a29bf8c2c3919ba477dddee64ccdee5
sknskin <sknskin@naver.com>
2022-12-13 17:55:21 +0900

    map update

---
+++ material-dashboard-react2/src/layouts/RentMap/index.js
@@ -0,0 +1,40 @@
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+
+import React, { useEffect } from "react";
+
+const { kakao } = window;
+
+function Kakao() {
+  
+  useEffect(() => {
+
+    const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+    const options = {
+      // 지도 생성 기본 옵션
+      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도 중심 좌표
+      level: 3, // 지도 레벨(확대, 축소 정도)
+    };
+    const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+    const markerPosition = new kakao.maps.LatLng(37.365264512305174, 127.10676860117488);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition,
+    });
+    marker.setMap(map);
+  }, []);
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <div
+        id="map"
+        style={{
+          width: "500px",
+          height: "500px",
+        }}
+      />
+    </DashboardLayout>
+  );
+}
+
+export default Kakao;
