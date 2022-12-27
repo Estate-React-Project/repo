09e719aa2ead0825b4968794b5bf42c05742a51c
BeYoungWu <byungwoo97@naver.com>
2022-12-27 13:09:50 +0900

    apt 마지막 수정

--- material-dashboard-react2/src/layouts/AptInfo/data/HeatPieChartData.js
+++ material-dashboard-react2/src/layouts/AptInfo/data/HeatPieChartData.js
@@ -1,5 +1,5 @@
 const makeHeatPieChartData = (data) => ({
-  labels: ["개별난방", "지역난방"],
+  labels: ["지역난방", "중앙난방", "개별난방", "기타"],
   datasets: {
     label: "아파트 난방 방식",
     backgroundColors: ["warning", "info", "error", "secondary"],
